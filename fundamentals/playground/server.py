9a24339c8ac23b896bf666e9f9b3cb801ff3cce4
Autumn Han <autumn9894@gmail.com>
2023-04-20 12:59:50 -0500

    adding /num route

--- fundamentals/playground/server.py
+++ fundamentals/playground/server.py
@@ -10,5 +10,9 @@ def home():
 def play():
     return render_template('play.html')
 
+@app.route('/<int:num>')
+def xrepeat(num):
+    return render_template(xrepeat.html, times=num)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
